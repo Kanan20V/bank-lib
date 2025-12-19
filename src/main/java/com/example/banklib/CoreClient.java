495d63da4fc2a4a912e8a11f964f4cf70114c6c7
valiyevkanan <kanan.verdiyev@kapitalbank.az>
2025-12-20 03:00:33 +0400

    fix error decoder error

--- src/main/java/com/example/banklib/CoreClient.java
+++ src/main/java/com/example/banklib/CoreClient.java
@@ -1,12 +1,13 @@
 package com.example.banklib;
 
+import com.example.banklib.error.FeignConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 
 @FeignClient(
     name = "core-client",
     url = "${api.core.url}",
     contextId = "core-client-context",
-    configuration = FeignClient.class
+    configuration = FeignConfig.class
 
 )
 public interface CoreClient extends CoreFacade {
